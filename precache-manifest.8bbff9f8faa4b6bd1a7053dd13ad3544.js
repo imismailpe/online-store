6fea1375a045448e4c341866cc674665a95fe70a
Ismail <mailismailpe@gmail.com>
2019-01-13 16:50:53 +0530

    Updates

---
+++ precache-manifest.8bbff9f8faa4b6bd1a7053dd13ad3544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985bec0124e4ffe2942",
+    "url": "/online-store/static/js/runtime~main.7985bec0.js"
+  },
+  {
+    "revision": "31f5322b75020d8f2a54",
+    "url": "/online-store/static/js/main.31f5322b.chunk.js"
+  },
+  {
+    "revision": "82b16e91b447676df9a4",
+    "url": "/online-store/static/js/1.82b16e91.chunk.js"
+  },
+  {
+    "revision": "31f5322b75020d8f2a54",
+    "url": "/online-store/static/css/main.48ef7057.chunk.css"
+  },
+  {
+    "revision": "82b16e91b447676df9a4",
+    "url": "/online-store/static/css/1.99e4e4ef.chunk.css"
+  },
+  {
+    "revision": "57f0cf5925aa027b14133781be8e634f",
+    "url": "/online-store/index.html"
+  }
+];
